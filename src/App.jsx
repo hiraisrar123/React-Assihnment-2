0c0b81fc0012f95dacb95fd58858cd6ae41ceafa
hiraisrar123 <hiraisrar47@gmail.com>
2026-01-19 22:16:32 +0500

    21hgfrds

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="hero">
+      <h1 className="logo">Marée</h1>
+
+      <p className="text">
+        Welcome to Marée, a brand new illustration & design portfolio
+        just waiting for your work to start populating its templates!
+      </p>
+
+      <button className="btn">PURCHASE</button>
+    </div>
+  );
+}
+
+export default App;
